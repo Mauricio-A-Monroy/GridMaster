033b5ef8c29c180c7b7cb62812423ff1cc1fac9f
Samuel Rojas <samu.02309@gmail.com>
2024-10-24 22:16:36 -0500

    Services and Controllers

---
+++ src/main/java/edu/co/arsw/gridmaster/service/UserService.java
@@ -0,0 +1,43 @@
+package edu.co.arsw.gridmaster.service;
+
+import edu.co.arsw.gridmaster.model.User;
+import edu.co.arsw.gridmaster.model.exceptions.GridMasterException;
+import edu.co.arsw.gridmaster.persistance.Tuple;
+import edu.co.arsw.gridmaster.persistance.UserPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserPersistence userPersistence;
+
+    public void move(String userName, Tuple<Integer, Integer> newPosition){
+        // void method for the moment
+    }
+
+    public User createUser(User user) throws GridMasterException {
+        userPersistence.saveUser(user);
+        return userPersistence.getUserByName(user.getUserName());
+    }
+
+    public void deleteUser(String name) throws GridMasterException {
+        userPersistence.deleteUser(name);
+    }
+
+    public void changeScore(String userName, Box box){
+        // void method for the moment
+    }
+
+    public Set<User> getAllUsers(){
+        return userPersistence.getAllUsers();
+    }
+
+    public User getUserByName(String name) throws GridMasterException {
+        return userPersistence.getUserByName(name);
+    }
+
+}
