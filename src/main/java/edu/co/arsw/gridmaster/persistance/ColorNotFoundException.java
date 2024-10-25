c4bf40ffc546b6ef60a62029a46c903b363ef674
Juan Contreras <judacobe2693@gmail.com>
2024-10-24 20:48:33 -0500

    color persistance

---
+++ src/main/java/edu/co/arsw/gridmaster/persistance/ColorNotFoundException.java
@@ -0,0 +1,13 @@
+package gridmaster.persistance;
+
+public class ColorNotFoundException extends Exception{
+
+    public ColorNotFoundException(String message) {
+        super(message);
+    }
+
+    public ColorNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ src/main/java/edu/co/arsw/gridmaster/persistance/ColorPersistanceException.java
@@ -0,0 +1,13 @@
+package gridmaster.persistance;
+
+public class ColorPersistanceException extends Exception{
+
+    public ColorPersistanceException(String message) {
+        super(message);
+    }
+
+    public ColorPersistanceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
